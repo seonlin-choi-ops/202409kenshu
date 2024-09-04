d64c2e5a441b4c5b509fb4485c25e3a27cf8dfae
seonlin_choi <seonlin_choi@openupsystem.co.jp>
2024-09-04 17:02:36 +0900

    show-5x5-matrix

---
+++ 202409kenshu/app/test/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+const array = [{ name: 1 }, { name: 2 }, { name: 3 }, { name: 4 }, { name: 5 }];
+
+export default function IndexPage() {
+    const [randomArray, setRandomArray] = useState<any[]>([]);
+    
+    useEffect(() => {
+        const randomizeArray : any[] = [...array].sort(() => 0.5 - Math.random());
+        setRandomArray(randomizeArray);
+    }, []);
+
+    console.log (randomArray)
+    return (
+        <div>
+            {randomArray.map((s, id) => (
+                <div key={id}>
+                    <h2>{s.name}</h2>
+                </div>
+            ))}
+        </div>
+    );
+}
